9e62ec67590b16b3b86606b7fc7351edf055ad1b
briesenberg07 <briesenb@uoregon.edu>
2025-02-10 11:35:12 -0800

    just use class/methods from IDLE for now

--- process.py
+++
@@ -1,6 +0,0 @@
-import Od2Package
-
-processing = Od2Package.Package()
-metadata = processing.get_files_md()[0]
-assets = processing.get_files_md()[1]
-processing.check_assets_filenames(metadata, assets)
