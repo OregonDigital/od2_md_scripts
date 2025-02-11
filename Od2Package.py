bad191f44dc5339c9b242c528065866533e97f70
briesenberg07 <briesenb@uoregon.edu>
2025-02-10 17:24:50 -0800

    comments on classes, methods, and one docstring

--- Od2Package.py
+++ Od2Package.py
@@ -14,11 +14,29 @@ class Package(object):
 
 
     def files_config(self):
+        """files_config() returs the paths entered in files_config.yaml.
+        These are returned as a list: 
+        [ 
+            path_to_metadata.csv,
+            path_to_files_directory 
+        ]"""
         with open(self.filesconf, "r") as yamlfile:
             paths = yaml.safe_load(yamlfile)
             return [ paths['metadata'], paths['assets'] ]
 
     
+    def default_config(self):
+        with open(self.defaultconf, "r") as yamlfile:
+            config = yaml.safe_load(yamlfile)
+        return config
+        
+
+    def coll_config(self):
+        with open(self.collconf, "r") as yamlfile:
+            config = yaml.safe_load(yamlfile)
+        return config
+
+
     def get_headers(self, metadata):
         with open(metadata, "r", encoding="utf-8-sig") as csvmetadata:
             reader = csv.DictReader(csvmetadata)
@@ -26,6 +44,36 @@ class Package(object):
         return headers
 
 
+    # (!) needs further testing (!)
+    # if pattern is in coll_config, I haven't figured out a way to 
+    # make the compile arg a raw string...
+    def check_identifier(self, csvfile, pattern): # add coll arg
+        print(f"regex pattern to match: {pattern}\nMalformed values if any:") # add "for coll (...)"
+        with open(csvfile, "r", encoding="utf-8-sig") as csvmetadata:
+            reader = csv.DictReader(csvmetadata)
+            p = re.compile(pattern) # this isn't a raw string... is that ok?
+            malformed = []
+            for row in reader:
+                if not re.match(pattern, row['identifier']):
+                    malformed.append(row['identifier'])
+        return malformed
+
+
+    def id_match_file(self, metadata): # uo-athletics
+        with open(metadata, "r", encoding="utf-8-sig") as csvmetadata:
+            reader = csv.DictReader(csvmetadata)
+            mismatch = []
+            for row in reader:
+                if row['identifier'] == row['file'].split('.')[0]:
+                    pass
+                else:
+                    mismatch.append(row['identifier'])
+        if len(mismatch) > 0:
+            return mismatch
+        else:
+            return "identifier values = filenames - file extension"
+
+    # check_identifier, id_match_file *before* this step
     # this would be different for complex objects!
     # this only works for simple objects!!
     def check_assets_filenames(self, metadata, assets):
@@ -65,44 +113,3 @@ class Package(object):
             print("filenames in metadata and files/ match")
         print('='*5)
 
-
-    def default_config(self):
-        with open(self.defaultconf, "r") as yamlfile:
-            config = yaml.safe_load(yamlfile)
-        return config
-        
-
-    def coll_config(self):
-        with open(self.collconf, "r") as yamlfile:
-            config = yaml.safe_load(yamlfile)
-        return config
-
-
-    def id_match_file(self, metadata):
-        with open(metadata, "r", encoding="utf-8-sig") as csvmetadata:
-            reader = csv.DictReader(csvmetadata)
-            mismatch = []
-            for row in reader:
-                if row['identifier'] == row['file'].split('.')[0]:
-                    pass
-                else:
-                    mismatch.append(row['identifier'])
-        if len(mismatch) > 0:
-            return mismatch
-        else:
-            return "identifier values = filenames - file extension"
-
-
-    # (!) needs further testing (!)
-    # if pattern is in coll_config, I haven't figured out a way to 
-    # make the compile arg a raw string...
-    def check_identifier(self, csvfile, pattern): # add coll arg
-        print(f"regex pattern to match: {pattern}\nMalformed values if any:") # add "for coll (...)"
-        with open(csvfile, "r", encoding="utf-8-sig") as csvmetadata:
-            reader = csv.DictReader(csvmetadata)
-            p = re.compile(pattern) # this isn't a raw string... is that ok?
-            malformed = []
-            for row in reader:
-                if not re.match(pattern, row['identifier']):
-                    malformed.append(row['identifier'])
-        return malformed
