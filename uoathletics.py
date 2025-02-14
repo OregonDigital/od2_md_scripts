60e7f9d95a12fcd0fca22cd8c1465bffaa195a74
briesenberg07 <riesenbergbenjamin@gmail.com>
2025-02-13 22:15:18 -0800

    work on uo-athletics-specific script

--- uoathletics.py
+++ uoathletics.py
@@ -1,16 +1,16 @@
-import yaml, os, csv
+import yaml, os, csv, re
 
 class UOAIngest(object):
 
 
     def __init__(self):
-        self.metadata = self.files_config()[0]
-        self.assets = os.listdir(self.files_config()[1])
+        self.metadata = self.filepaths()[0]
+        self.assets = os.listdir(self.filepaths()[1])
         self.config = self.fields_config()
 
 
-    def files_config(self):
-        with open("files_config.yaml", "r") as yamlfile:
+    def filepaths(self):
+        with open("filepaths.yaml", "r") as yamlfile:
             paths = yaml.safe_load(yamlfile)
             return [ paths['metadata'], paths['assets'] ]
 
@@ -20,13 +20,65 @@ class UOAIngest(object):
             config = yaml.safe_load(yamlfile)
             return(config)
     
-    def all_csv_processing(self):
+    def csv_columns_processing(self):
         with open(self.metadata, "r", encoding="utf-8-sig") as csvfile:
             reader = csv.DictReader(csvfile)
             headers = reader.fieldnames
             for header in headers:
-                if header in self.config:
-                    print(f"config for header '{header}': {self.config[header]}")
+                # test
+                # try:
+                #     print(self.config[header][0])
+                # except:
+                #     print(f"{header} not in uo-athletics_config.yaml")
+                # punting on file config, currently only config set as func
+                if header in self.config and header != 'file':
+                    print(f"checking column {header}\n{'='*5}")
+                    if self.config[header][0] == 'function':
+                        pass
+                    elif self.config[header][0] == 'string':
+                        for row in reader:
+                            if row[header] == self.config[header][1]:
+                                pass
+                            else:
+                                print(f"correct value '{row[header]}'")
+                    elif self.config[header][0] == 'regex':
+                        for row in reader:
+                            if not re.match(self.config[header][1], row[header]):
+                                print(f"correct value {row[header]}")
                 else:
-                    print(f"no config data for header '{header}'")
-                    
+                    print(f"no check configured for header {header}\n{'='*5}")
+
+
+    def csv_filenames_assets_check(self):
+        print(f"checking metadata filenames against files/ assets\n{'='*5}")
+        with open(self.metadata, "r", encoding="utf-8-sig") as csvfile:
+            reader = csv.DictReader(csvfile)
+            filenames = []
+            for row in reader:
+                filenames.append(row['file'])
+            difflen = len(self.assets) - len(filenames)
+            if difflen != 0:
+                print("ERROR: # of filenames != # of asset files:")
+                print(f"{len(filenames)} filename values in CSV metadata")
+                print(f"{len(self.assets)} files in assets directory")
+            else:
+                print("# of filenames = # of asset files")
+            # this check may still be wonky
+            # (!) need to understand set better -- why am I using set here?
+            # would diffs below work just as well with simple len() comparisons?
+            diff = list(set(self.assets) - set(filenames))
+            if len(diff) > 0:
+                print(f"{len(diff)} files/ assets not in metadata\n{'*'*5}")
+                for item in diff:
+                    print(item)
+                print("\n")
+            else:
+                print("files/ and metadata filenames match")
+            diff = list(set(filenames) - set(self.assets))
+            if len(diff) > 0:
+                print(f"{len(diff)} filenames not in files/ assets\n{'*'*5}")
+                for item in diff:
+                    print(item)
+                print("\n")
+            else:
+                print("metadata filenames and files/ match")
