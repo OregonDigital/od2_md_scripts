21c88a4561652206f8de772a0e5b9ddf001be201
briesenberg07 <riesenbergbenjamin@gmail.com>
2025-02-15 21:02:35 -0800

    add prompt to provide config file name

--- process.py
+++ process.py
@@ -1,13 +1,13 @@
 import sys
 from od2ingest import Ingest
 
-if sys.argv[1]:
+try:
     processing = Ingest(sys.argv[1])
     processing.process_columns()
     files_check = input("check metadata filenames against files/ assets? (y/n)\n>>>")
     if files_check.lower() == 'y':
         processing.check_filenames_assets()
-    else:
-        pass
-else:
-    print("missing: config file name (minus file format extension)")
+except IndexError:
+    print(f"(!) MISSING config file name (do not include file extension)")
+    print("EXAMPLE command:")
+    print("> python process.py uo-athletics")
