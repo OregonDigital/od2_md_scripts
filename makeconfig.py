a4686af8de144c370e0ea9a23eaa04aa30d3c280
briesenberg07 <riesenbergbenjamin@gmail.com>
2025-03-18 22:03:06 -0700

    work on #1 and testing, notes

--- makeconfig.py
+++ makeconfig.py
@@ -13,8 +13,15 @@ config = {}
 for entry in headers:
     config.update({entry: None})
 
-print("enter a name for your config file, including the file extension '.yaml', for example:")
-print(">>>archival-materials.yaml")
+# testing
+# print(type(config))
+# pretty = json.dumps(config, indent=4)
+# print(pretty)
+# with open("test.yaml", "w+") as yamlfile:
+#     yaml.safe_dump(config, yamlfile, encoding="utf-8")
+
+print("enter a name for your config file, excluding file extension, for example:")
+print(">>>archival-materials")
 print("enter 'n' to exit")
 
 def get_input():
@@ -27,14 +34,13 @@ while filename is None:
     filename = get_input()
     if filename.lower() == 'n':
         exit()
-    elif filename in os.listdir("config"):
-        print("a file with that name already exists in od2_md_scripts/config/")
+    elif f"{filename}.yaml" in os.listdir("config"):
+        print("a config file with that name already exists in od2_md_scripts/config/")
         filename = None
-    elif filename[-5:] != '.yaml':
-        print("file name must include file extension '.yaml'")
+    elif filename == '':
+        print("enter filename, or 'n' to exit")
         filename = None
     else:
-        print("enter '[filename].yaml' or 'n' to exit")
         pass
 
 print("(!) your config template file will be created as follows:")
@@ -46,11 +52,10 @@ for header in config:
 while True:
     confirmation = input("proceed to create config template file? (y/n)\n>>>").lower()
     if confirmation == 'y':
-        jsonconfig = json.load(config)
-        yaml.safe_load(jsonconfig)
-        with open(f"config/{filename}", "w+") as yamlfile:
-            yaml.safe_dump(jsonconfig)
-        print("config template ready")
+        with open(f"config/{filename}.yaml", "w+") as yamlfile:
+            yaml.safe_dump(config, yamlfile, encoding='utf-8')
+        print("config template ready:")
+        print(f"od2_md_scripts/config/{filename}.yaml")
         break
     elif confirmation == 'n':
         break
