d62d78a03e3c3888104747edcb6fc06ebfbb84fb
briesenberg07 <riesenbergbenjamin@gmail.com>
2025-06-10 22:00:19 -0700

    update config file structure

--- makeconfig.py
+++ makeconfig.py
@@ -1,20 +1,37 @@
 import csv, json, yaml, os
+import pandas as pd
 
-print("enter absolute filepath to .csv metadata file, for example:")
-print(">>>C:\\Users\\briesenb\\Desktop\\metadata.csv")
+print("enter either (1) or (2)")
+print("(1) absolute filepath to .csv metadata file, as:")
+print(">>> C:\\Users\\briesenb\\Desktop\\metadata.csv")
+print("(2) absolute filepath to .xlsx metadata file followed by space and sheet name, as:")
+print(">>> C:\\Users\\briesenb\\Desktop\\metadata.xlsx Sheet1")
 
-file = input(">>>")
+file = input(">>> ")
 
-with open(file, "r") as csvfile:
-    reader = csv.DictReader(csvfile)
-    headers = reader.fieldnames
+if len(file.split()) > 1 and file.split()[0].split('.')[-1] == "xlsx":
+    filepath = file.split()[0]
+    sheet_name = file.split()[1]
+    df = pd.read_excel(filepath, sheet_name=sheet_name)
+    headers = df.columns.to_list()
+elif len(file.split()) == 1 and file.split('.')[-1] == "csv":
+    with open(file, "r", encoding="utf-8-sig") as csvf:
+        reader = csv.reader(csvf)
+        headers = next(reader)
+else:
+    print("(!) wrong number of input strings and/or unknown metadata file extension")
+    exit()
 
 config = {}
 for entry in headers:
-    if entry == "file":
-        config.update({entry: ["method", "check_filenames_assets"]})
+    if entry == "dmrec":
+        config.update({entry: [{"string": ""}]})
+    elif entry == "file":
+        config.update({entry: [{"method": ["check_filenames_assets"]}]})
+        # ^^^ NEEDS UPDATE -- how to pass method info in config!?
     else:
         config.update({entry: None})
+    # TO DO -- REMOVE ABOVE once default config for dmrec and file are in place
 
 # testing
 # print(type(config))
@@ -24,11 +41,11 @@ for entry in headers:
 #     yaml.safe_dump(config, yamlfile, encoding="utf-8")
 
 print("enter a name for your config file, excluding file extension, for example:")
-print(">>>archival-materials")
+print(">>> archival-materials")
 print("enter 'n' to exit")
 
 def get_input():
-    user_input = input(">>>")
+    user_input = input(">>> ")
     return user_input
 
 filename = None
@@ -46,14 +63,14 @@ while filename is None:
     else:
         pass
 
-print("(!) your config template file will be created as follows:")
-print(f"***path + file name: od2_md_scripts/config/{filename}.yaml")
-print(f"***metadata fields (headers) -- {len(config)} total:")
+print("(*) your config file will be created as follows:")
+print(f"***path + file name: ../od2_md_scripts/config/{filename}.yaml")
+print(f"***{len(config)} metadata fields (headers):")
 for header in config:
     print(f"\t{header}")
 
 while True:
-    confirmation = input("proceed to create config template file? (y/n)\n>>>").lower()
+    confirmation = input("proceed to create config template file? (y/n)\n>>> ").lower()
     if confirmation == 'y':
         with open(f"config/{filename}.yaml", "w+") as yamlfile:
             yaml.safe_dump(config, yamlfile, encoding='utf-8')
