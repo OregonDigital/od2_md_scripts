aa9bf22ee0405b48a5ed71e4f15c5f658a20eeeb
briesenberg07 <riesenbergbenjamin@gmail.com>
2025-07-01 20:05:34 -0700

    first draft new process.py

--- process.py
+++ process.py
@@ -1,3 +1,12 @@
+from od2validation import Package
+import sys
 
-# to do
-# include print_filenames()
+try:
+    processing = Package(sys.argv[1])
+    # print(type(processing)) # check
+    processing.print_filepaths()
+    processing.check_headers()
+    processing.get_headers_instructions()
+except IndexError:
+    print("(!!) command missing config file name (do not include file extension)")
+    print("EXAMPLE:\n> python3 process.py uo-athletics")
