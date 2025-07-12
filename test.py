97c64b089313da6c6d1474e67da36e014b991a50
briesenberg07 <riesenbergbenjamin@gmail.com>
2025-07-11 18:23:21 -0700

    very minor

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 from od2validation import Package
 
 # requires od2_md_scripts/filepaths_test.yaml, 
-# which is .gitignored and may not exist locally,
-# as:
+# which is .gitignored and may not exist locally, as:
 # metadata:
 #   - /path/to/od2_md_scripts/test/test.xlsx|csv
 # assets: /path/to/od2_md_scripts/test/files
