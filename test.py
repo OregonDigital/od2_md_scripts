e98d9ced201a65da4d1de3c544421917a27f8135
briesenberg07 <riesenbergbenjamin@gmail.com>
2025-07-11 18:22:52 -0700

    comment re: needed YAML file

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 from od2validation import Package
 
+# requires od2_md_scripts/filepaths_test.yaml, 
+# which is .gitignored and may not exist locally,
+# as:
+# metadata:
+#   - /path/to/od2_md_scripts/test/test.xlsx|csv
+# assets: /path/to/od2_md_scripts/test/files
+
 test = Package("test", True)
 test.print_filepaths()
 test.print_config()
