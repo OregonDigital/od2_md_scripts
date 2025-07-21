c3c8bb70ce519a0355f407511ad0f6b8c93ebcc1
briesenberg07 <riesenbergbenjamin@gmail.com>
2025-07-21 12:50:15 -0700

    notes cleanup

--- notes/sys_argv.py
+++
@@ -1,6 +0,0 @@
-import sys
-# can't remember if 'python'/'python3' is counted in indexing
-# run $ 'python3 sys_argv.py' + arbitrary number of additional arbitrary args
-print("arguments indexed by sys.argv:")
-for arg in sys.argv:
-    print(f"{sys.argv.index(arg)}: {arg}")
