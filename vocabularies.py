ceab7e239a14177ea093b31329cedcc1fa10ee6d
Cole Herman <colesh@uoregon.edu>
2026-02-17 14:40:13 -0800

    Default validation for Creator controlled vocabulary

--- vocabularies.py
+++ vocabularies.py
@@ -19,37 +19,77 @@ def validate_lcnaf(value: str) -> bool:
     return bool(re.match(pattern, value))
 
 def validate_ulan(value: str) -> bool:
-    """Validate ULAN URI format
+    """Validate ulan URI format
     
     Examples:
+    solr query: "http://vocab.getty.edu/ulan/500012467"
+    solr query: "http://vocab.getty.edu/ulan/500006931"
+    solr query: "http://vocab.getty.edu/ulan/500330183"
+    od2 map: "http://vocab.getty.edu/ulan/500009666"
     
-    
+    Match start through ulan/, then 500, then 6 integers
     """
     # Examples
-    pattern = r''
+    pattern = r'http:\/\/vocab.getty.edu\/ulan\/500\d{6}'
     return bool(re.match(pattern, value))
 
 def validate_creator(value: str) -> bool:
-    """Validate creator URI format"""
-    pattern = r''
+    """Validate creator URI format
+    
+    Examples:
+    solr query: "http://opaquenamespace.org/ns/creator/HoldenDorbe"
+    solr query: "http://opaquenamespace.org/ns/creator/Nexus"
+    solr query: "http://opaquenamespace.org/ns/creator/OlsenandJohnson"
+    solr query: "http://opaquenamespace.org/ns/creator/BennesJohnV"
+    solr query: "http://opaquenamespace.org/ns/creator/OpsisArchitecturearchitecturalfirm"
+    od2 map: "http://opaquenamespace.org/ns/creator/HaynesCharles"
+    
+    Match start through creator/, then any string with length > 0
+    """
+    pattern = r'http:\/\/opaquenamespace.org\/ns\/creator\/[a-zA-Z]+'
     return bool(re.match(pattern, value))
 
 def validate_people(value: str) -> bool:
-    """Validate lcnaf URI"""
-    pattern = r''
+    """Validate people URI
+    
+    Examples:
+    solr query: "http://opaquenamespace.org/ns/people/SkinnerJamesEdward18671959"
+    solr query: "http://opaquenamespace.org/ns/people/SkinnerSusanLawrence18711952"
+    solr query: "http://opaquenamespace.org/ns/people/SteinkeClaytonEJr"
+    od2 map: "http://opaquenamespace.org/ns/people/GrayKen"
+
+    Match start through people/, then any number of digits or letters with length > 0
+    """
+    pattern = r'http:\/\/opaquenamespace.org\/ns\/people\/[a-zA-Z\d]+'
     return bool(re.match(pattern, value))
 
 def validate_wikidata(value: str) -> bool:
-    """Validate ULAN URI format"""
-    pattern = r''
+    """Validate wikidata URI format
+    
+    Examples:
+    from website: "http://www.wikidata.org/entity/Q193020"
+    od2 map: "http://www.wikidata.org/entity/Q6134558"
+    
+    Match start through entity/, then Q, then digits
+    """
+    pattern = r'http:\/\/www\.wikidata\.org\/entity\/Q\d+'
     return bool(re.match(pattern, value))
 
 def validate_osuacademicunits(value: str) -> bool:
-    """Validate creator URI format"""
-    pattern = r''
+    """Validate osuacademicunits URI format
+    
+    Examples:
+    from website: "http://opaquenamespace.org/ns/osuAcademicUnits/5eh7OKFX"
+    from website: "http://opaquenamespace.org/ns/osuAcademicUnits/Sp8GIq9b"
+    from website: "http://opaquenamespace.org/ns/osuAcademicUnits/LaningEnosRolandJr"
+    od2 map: "http://opaquenamespace.org/ns/osuAcademicUnits/smuGLIjL"
+    
+    Match start through osuAcademicUnits/, then any number of digits or letters with length > 0 
+    """
+    pattern = r'http:\/\/opaquenamespace\.org\/ns\/osuAcademicUnits\/[a-zA-Z\d]+'
     return bool(re.match(pattern, value))
 
-# Add rest here
+# Add rest here (from under each subfield of fields in controlled_vocab_map, in validation_mappings.yaml)
 
 
 
