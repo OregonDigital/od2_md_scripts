6ccad3e2794ef8255d681b7e86d554d288aae137
Cole Herman <colesten@gmail.com>
2026-07-24 16:02:38 -0700

    Remove incorrect FIXME and switch to id over identifier, since that's correct export label

--- make_id_list.py
+++ make_id_list.py
@@ -7,8 +7,7 @@ def main():
     filepath = input("Enter filepath to csv: ")
     with open(filepath, "r", encoding="utf-8-sig") as csvf:
             reader = csv.DictReader(csvf)
-            #FIXME: This won't ignore if a complex ID is in the column
-            unformatted_ids = [row["identifier"] for row in reader]
+            unformatted_ids = [row["id"] for row in reader]
     formatted_ids = '|'.join(unformatted_ids)
     print(formatted_ids)
 
