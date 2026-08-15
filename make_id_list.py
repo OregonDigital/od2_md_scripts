6595fd3fc4c9ff4df87dc8ba45ccb57d5d85ed92
coleherman43 <colesten@gmail.com>
2026-08-14 19:15:14 -0700

    Skip complex object ids and detect blank ids

--- make_id_list.py
+++ make_id_list.py
@@ -1,15 +1,23 @@
 """Generate pipe-separated list of files"""
 import csv, os
+from utils import is_complex
 
 # Could make a function that does this outside of main, so that you can call that to automate this whole part of the process
 
-#FIXME: Currently includes complex object id in the list. Skip this using is_complex from utils
 def main():
-    filepath = input("Enter filepath to csv: ")
+    filepath = input("Enter filepath to csv\n>>> ")
     with open(filepath, "r", encoding="utf-8-sig") as csvf:
             reader = csv.DictReader(csvf)
-            # Should be "id" column from export by default, but possible to change this if needed
-            unformatted_ids = [row["id"] for row in reader]
+            unformatted_ids = []
+
+            for row in reader:
+                # Should be "id" column from export by default, but possible to change this if needed
+                row_id = (row.get("id", "")).strip()
+                if not row_id:
+                     raise ValueError(f"Blank id found at row {reader.line_num}")
+                if not is_complex(row):
+                     unformatted_ids.append(row_id)
+            
     formatted_ids = '|'.join(unformatted_ids)
     print(formatted_ids)
 
