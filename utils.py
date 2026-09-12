599f3d48a411f0433a4e9c99e055326f5497e634
coleherman43 <colesten@gmail.com>
2026-09-11 17:42:55 -0700

    Write is_complex_solr, determine complexity from solr query

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import re
+import sys, requests
 
 COMPLEX_MODEL_VALUE = "generic"
 COMPLEX_RESOURCE_TYPE_VALUE = "http://purl.org/dc/dcmitype/Collection"
@@ -15,6 +16,12 @@ def is_complex(row) -> bool:
     model = str(row.get("model")).lower()
     resource = str(row.get("resource_type")).lower()
     return model == COMPLEX_MODEL_VALUE.lower() and resource == COMPLEX_RESOURCE_TYPE_VALUE.lower()
+
+def is_complex_solr(work) -> bool:
+    """Return True if work is complex, False otherwise"""
+    model = work.get("has_model_ssim", [])
+    resource_type = work.get("resource_type_label_ssim", [])
+    return "Generic" in model and "Collection" in resource_type
 
 def is_fileset(row) -> bool:
     """Return True if row is a file set, False otherwise"""
